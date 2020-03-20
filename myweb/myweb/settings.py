ced538f6d6d33f848a25356946230c2255c15b39
Yct <vincent870414@gmail.com>
2020-03-20 22:49:22 +0800

    Init

--- myweb/myweb/settings.py
+++ myweb/myweb/settings.py
@@ -74,7 +74,7 @@ WSGI_APPLICATION = 'myweb.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES = {
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
