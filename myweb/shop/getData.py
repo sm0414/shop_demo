26541fbd2d505bac156d63ddec81d46640b7a04e
Yct <vincent870414@gmail.com>
2020-03-20 23:36:24 +0800

    Init

--- myweb/shop/getData.py
+++ myweb/shop/getData.py
@@ -5,10 +5,10 @@ Created on Tue Mar 10 11:10:49 2020
 @author: vincent
 """
 
-from . import scraping_price
-from . import scraping_accuracy
-#import scraping_price
-#import scraping_accuracy
+#from . import scraping_price
+#from . import scraping_accuracy
+import scraping_price
+import scraping_accuracy
 
 def data_price(product):
     yahoo_data = scraping_price.search_yahoo(product)
