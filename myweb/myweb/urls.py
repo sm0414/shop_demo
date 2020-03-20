d54d7f4c0c5a1d487b0e574bef8e807a5ceeaa1c
Yct <vincent870414@gmail.com>
2020-03-20 23:02:57 +0800

    Init

--- myweb/myweb/urls.py
+++ myweb/myweb/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.contrib import staticfiles
 
 from shop.views import index
 
