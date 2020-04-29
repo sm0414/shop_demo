05220cce69279ed67c69eed2550da9892dbf6d59
Yct <vincent870414@gmail.com>
2020-04-29 17:02:57 +0800

    Init

--- myweb/shop/getData.py
+++ myweb/shop/getData.py
@@ -5,26 +5,26 @@ Created on Tue Mar 10 11:10:49 2020
 @author: vincent
 """
 
-#from . import scraping_price
-#from . import scraping_accuracy
-import scraping_price
-import scraping_accuracy
+from . import scraping_price
+from . import scraping_accuracy
+#import scraping_price
+#import scraping_accuracy
 
 def data_price(product):
     yahoo_data = scraping_price.search_yahoo(product)
     momo_data = scraping_price.search_momo(product)
     pchome_data = scraping_price.search_pchome(product)
-    
+
     alldata = []
-    
+
     for y,m,p in zip(yahoo_data,momo_data,pchome_data):
         alldata.append(y)
         alldata.append(m)
         alldata.append(p)
-    
-    
+
+
     contents = sorted(alldata, key=lambda i: i['price'])
-    
+
     return contents
 
 
@@ -33,17 +33,17 @@ def data_accuracy(product):
     momo_data = scraping_accuracy.search_momo(product)
     pchome_data = scraping_accuracy.search_pchome(product)
 
-    
+
     alldata = []
-    
+
     for y,m,p in zip(yahoo_data,momo_data,pchome_data):
         alldata.append(y)
         alldata.append(m)
         alldata.append(p)
-    
-    
+
+
 #    contents = sorted(alldata, key=lambda i: i['price'])
-    
+
     return alldata
 
 #print(data_accuracy('口罩'))
