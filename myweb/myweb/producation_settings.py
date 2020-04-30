6cb45895f05278ee6d1a7d1e9c76808892120836
Yct <vincent870414@gmail.com>
2020-04-30 17:35:52 +0800

    Init

--- myweb/myweb/producation_settings.py
+++ myweb/myweb/producation_settings.py
@@ -8,7 +8,7 @@ DATABASES = {
 
 # Static asset configuration
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
